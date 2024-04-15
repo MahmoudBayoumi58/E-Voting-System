33030a60c396ec1304574fe7f26fa50ff9d55149
Mahmoud Bayoumi <64416945+MahmoudBayoumi58@users.noreply.github.com>
2024-04-15 05:50:02 +0200

    Update settings.py
    remove private account for sending email

--- backend/settings.py
+++ backend/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zyadtiktok91@gmail.com'
-EMAIL_HOST_PASSWORD = 'unrx hhqq mytd jntb'
+EMAIL_HOST_USER = '**********'
+EMAIL_HOST_PASSWORD = '*********'
